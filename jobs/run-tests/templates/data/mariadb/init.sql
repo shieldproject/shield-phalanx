11e8dc9076ca2252a3a152325319d32b4c6ea5d8
James Hunt <james@niftylogic.com>
2017-02-04 01:45:21 -0500

    Set up Phalanx for primary plugins
    - MySQL
    - Postgres
    - Consul
    - Mongo
    - FS

---
+++ jobs/run-tests/templates/data/mariadb/init.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS db1;
+CREATE DATABASE db1;
+USE db1;
+
+CREATE TABLE t_vals (
+  id    INTEGER,
+  value VARCHAR(100)
+);
+INSERT INTO t_vals (id, value) VALUES (1, 'faip9aexe7dohGheemiemah3ohngoi5eiwoo6sooKooxie4Cifairiewemuuphie');
+INSERT INTO t_vals (id, value) VALUES (2, 'iXoop3Sah3eopool7Kee6ohThooquu7pheing5oveutaimue2thooPhohchaeC0x');
+INSERT INTO t_vals (id, value) VALUES (3, 'zeigheiphic3Bahzu2iehaidootheix0eeveeGhinuQu1iey6weeth4chook0Aex');
+INSERT INTO t_vals (id, value) VALUES (4, 'oj8eejadah6eefaed7kohs6ue1aaphaingoi9eduiw8Te5ogoh4AilouchohL7va');
+
+--------------------------------------
+
+DROP DATABASE IF EXISTS db2;
+CREATE DATABASE db2;
+USE db2;
+
+CREATE TABLE t_keys (
+  id    INTEGER,
+  value VARCHAR(100)
+);
+INSERT INTO t_keys (id, value) VALUES (5, 'zeiqu8ci6mae1uJ0doh7gitechooZieweeyaaPoh0Tegheijabei7ma4Chea9koo');
+INSERT INTO t_keys (id, value) VALUES (6, 'yo6Eipiewee2Rahc2shiebai1ASheih6giaYae9feew3Woon5iep2lu6nooQuio2');
+INSERT INTO t_keys (id, value) VALUES (7, 'iekish9feu9ahqu6aing0pheiNg5que2aec6aeXahkoh5fie7ochoomoothepixo');
+INSERT INTO t_keys (id, value) VALUES (8, 'Xo4moosie5gakurae0phaeYieshi0kith7aingahchut5iex5ceewaineehaishu');
